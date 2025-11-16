6a6b193d4cb4e60d773cc4c08b116c74693927f9
George Crisan <georgerdp@gmail.com>
2025-11-16 17:29:34 +0000

    feat: move logic to channel

--- main.go
+++ main.go
@@ -1,5 +1,78 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func getLinesChannel(fileData io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer fileData.Close()
+		defer close(out)
+
+		result := ""
+
+		for {
+			// prepare a slike of 8 bytes
+			data := make([]byte, 8)
+
+			// read a bunch of data in that slice
+			n, err := fileData.Read(data)
+
+			if n == 0 {
+				break
+			}
+
+			if err != nil {
+				fmt.Printf("Unexpected error %s", err)
+			}
+
+			// Slice to actually number of bytes populated
+			// although we want to populat 8 bytes it could be the case less than 8 were available
+			data = data[:n]
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				result += string(data[:i])
+
+				if len(result) > 0 {
+					toPrint := strings.ReplaceAll(result, "\n", "")
+					out <- toPrint
+
+				}
+				result = ""
+				data = data[i+1:]
+			}
+
+			result += string(data)
+		}
+
+		if i := len(result); i > 0 {
+			out <- result
+		}
+	}()
+
+	return out
+}
+
 func main() {
-	println("Start")
+	fileData, err := os.Open("./assets/byte-data.txt")
+
+	if err != nil {
+		log.Fatal("Encountered error: ", err)
+	}
+
+	lines := getLinesChannel(fileData)
+
+	count := 1
+	for line := range lines {
+		fmt.Println("Line " + fmt.Sprint(count) + ": " + line)
+		count++
+	}
+
 }
